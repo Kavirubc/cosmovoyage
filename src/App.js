c55adf07ea8b59e4ff8d420fb0cd827d2c9afded
Kaviru Hapuarachchi <hapuarachchikaviru@gmail.com>
2023-08-17 18:33:30 +0530

    MUI and FlowBite

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import VerificationPage from './pages/VerificationPage';
 import EmailVerification from './pages/EmailVerification';
 import MobileVerification from './pages/MobileVerification';
+import SeatBooking from './pages/SeatBooking';
 
 function App() {
   return (
     <div className="App">
       <MobileVerification />
+      <SeatBooking />
+
     </div>
   );
 }
-
 export default App;
