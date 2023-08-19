d36b9390a9a965774c39bbded20cdb032aa8144e
Kenura-R-Gunarathna <kenuragunarathna@gmail.com>
2023-08-19 12:49:42 +0530

    auth middleware added

--- src/utils/CryptoJS.js
+++ src/utils/CryptoJS.js
@@ -4,14 +4,14 @@ import CryptoJS from "crypto-js";
 export const encryptData = (text) => {
   const data = CryptoJS.AES.encrypt(
     JSON.stringify(text),
-    process.env.REACT_APP_SECRET_PASs
+    process.env.REACT_APP_SECRET_PASS
   ).toString();
 
   return data;
 };
 
 export const decryptData = (text) => {
-  const bytes = CryptoJS.AES.decrypt(text, process.env.REACT_APP_SECRET_PASs);
+  const bytes = CryptoJS.AES.decrypt(text, process.env.REACT_APP_SECRET_PASS);
   const data = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
   return data;
 };
