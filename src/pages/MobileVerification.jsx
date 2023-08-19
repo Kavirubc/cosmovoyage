e80b3bc249a8bb14a33fd022aa233ed181211b27
Kenura-R-Gunarathna <kenuragunarathna@gmail.com>
2023-08-19 14:34:24 +0530

    mobile route image css fixed

--- src/pages/MobileVerification.jsx
+++ src/pages/MobileVerification.jsx
@@ -1,21 +1,20 @@
-import VerificationPage  from "./VerificationPage";
+import VerificationPage from "./VerificationPage";
 import "../App.css";
 import NavBar from "../components/molecules/NavBar";
 
-var phone ="+9470 444 4444";
+var phone = "+9470 444 4444";
 
 const MobileVerification = () => {
-    return (
-      <div className="App">
-        <VerificationPage titleAlign="left" imageType="Mobile">
-          <h2>Verify Your Mobile</h2>
-          <p>A  5 - digit code has been sent to</p>
-          <p><b>{phone}</b> <a href="#">Change</a></p>
-        </VerificationPage>
-        <NavBar />
-      </div>
-    );
+  return (
+    <div className="mobile-container">
+      <VerificationPage titleAlign="left" imageType="Mobile">
+        <h2>Verify Your Mobile</h2>
+        <p>A  5 - digit code has been sent to</p>
+        <p><b>{phone}</b> <a href="/">Change</a></p>
+      </VerificationPage>
+      <NavBar />
+    </div>
+  );
 }
 
 export default MobileVerification;
-  
