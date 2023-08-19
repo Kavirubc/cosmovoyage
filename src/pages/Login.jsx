7f66dae6b083334a71f1e3e614f2fe9a8ee46551
Kenura-R-Gunarathna <kenuragunarathna@gmail.com>
2023-08-19 14:15:28 +0530

    signup page added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -54,6 +54,9 @@ const Login = () => {
       })
       .catch(function (error) {
 
+        document.querySelector("#username-error").innerHTML = "";
+        document.querySelector("#password-error").innerHTML = "";
+
         let output = error.response.data;
 
         ErrorManager(output);
@@ -65,7 +68,7 @@ const Login = () => {
     <div>
 
       <section className="bg-gray-50 dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
           <a
             href="/"
             className="flex items-center mb-6 mt-20 text-2xl font-semibold text-gray-900 dark:text-white"
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -10,20 +10,33 @@ import ErrorManager from '../utils/ErrorManager';
 const SignUp = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [terms, setTerms] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [gender, setGender] = useState('');
 
 
   const handleLogin = (event) => {
     event.preventDefault();
 
     /* 
-    * Send a POST request for signup
+    * Send a POST request for signupemail
     */
     axios({
       method: 'post',
       url: process.env.REACT_APP_API + '/api/v1/user/register',
       data: {
         username: username,
-        password: password
+        password: password,
+        confirm_password: confirmPassword,
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        contact_number: contactNumber,
+        gender: gender,
       }
     })
       .then(function (response) {
@@ -46,6 +59,7 @@ const SignUp = () => {
 
           document.querySelector("#username-error").innerHTML = "";
           document.querySelector("#password-error").innerHTML = "";
+          document.querySelector("#confirm_password-error").innerHTML = "";
 
           console.log("User Logged In sucessfull!");
         }
@@ -63,7 +77,7 @@ const SignUp = () => {
     <div>
 
       <section className="bg-gray-50 dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
           <a
             href="/"
             className="flex items-center mb-6 mt-20 text-2xl font-semibold text-gray-900 dark:text-white"
@@ -78,9 +92,10 @@ const SignUp = () => {
           <div className="w-full sm:bg-white rounded-lg shadow-none sm:shadow dark:border md:mt-0 mb-20 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign up to your account
+                Create an account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form className="space-y-4 md:space-y-6">
+
                 <div>
                   <label
                     htmlFor="username"
@@ -100,6 +115,7 @@ const SignUp = () => {
                   />
                   <span className="text-red-600 text-xs pt-1" id="username-error"></span>
                 </div>
+
                 <div>
                   <label
                     htmlFor="password"
@@ -119,39 +135,147 @@ const SignUp = () => {
                   />
                   <span className="text-red-600 text-xs pt-1" id="password-error"></span>
                 </div>
-                <div className="flex items-center justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                        required=""
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label
-                        htmlFor="remember"
-                        className="text-gray-500 dark:text-gray-300"
-                      >
-                        Remember me
-                      </label>
-                    </div>
+                
+                <div>
+                  <label
+                    htmlFor="confirm-password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Confirm password
+                  </label>
+                  <input
+                    type="confirm-password"
+                    name="password_confirmation"
+                    id="confirm-password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                  />
+                  <span className="text-red-600 text-xs pt-1" id="confirm_password-error"></span>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="firstname"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your first name
+                  </label>
+                  <input
+                    type="text"
+                    name="firstname"
+                    id="firstname"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="E.g.: Jhon"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    required
+                  />
+                  <span className="text-red-600 text-xs pt-1" id="firstname-error"></span>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="lastname"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your last name
+                  </label>
+                  <input
+                    type="text"
+                    name="lastname"
+                    id="lastname"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="E.g. : Doe"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                  />
+                  <span className="text-red-600 text-xs pt-1" id="lastname-error"></span>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="contact-number"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your contact number
+                  </label>
+                  <input
+                    type="text"
+                    name="contact-number"
+                    id="contact-number"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="94-777-190-590"
+                    value={contactNumber}
+                    onChange={(e) => setContactNumber(e.target.value)}
+                    required
+                  />
+                  <span className="text-red-600 text-xs pt-1" id="contact_number-error"></span>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required=""
+                  />
+                </div>
+
+                <div>
+                  <label for="gender" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select your gender</label>
+                  <select id="gender" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected disabled>Select your gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Femal</option>
+                    <option value="rather_not_say">Rather not say</option>
+                    <option value="other">Other</option>
+                  </select>
+
+                </div>
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                      checked={terms}
+                      onChange={(e) => setTerms(!terms)}
+                      required
+                    />
                   </div>
-                  {/* <a
-                    href="/"
-                    className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  <div className="ml-3 text-sm">
+                    <label
+                      htmlFor="terms"
+                      className="font-light text-gray-500 dark:text-gray-300"
                     >
-                      Forgot password?
-                  </a> */}
+                      I accept the&nbsp;
+                      <span className="font-medium text-blue-600 hover:underline dark:text-blue-500">
+                        Terms and Conditions
+                      </span>
+                    </label>
+                  </div>
                 </div>
                 <button
                   type="submit"
                   onClick={handleLogin}
                   className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Sign up
+                  Create an account
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Already have an account?&nbsp;&nbsp;
